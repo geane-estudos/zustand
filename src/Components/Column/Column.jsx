ede58ff835b3e12d23eeab033ad46756e37f80b0
geanemr <geane.ramos@contavoltz.com>
2023-12-04 15:43:17 -0300

    chore: store creation

--- src/Components/Column/Column.jsx
+++ src/Components/Column/Column.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import './Column.css'
 import Task from '../Task/Task'
+import { useStore } from '../../../store'
 
 const Column = ({state}) => {
+  const tasks = useStore(store=>store.tasks.filter(task=>task.state === state))
   return (
     <div className='column'>
       <div>{state}</div>
