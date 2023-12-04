b07a5ed4b0993f0c2014050d134bd4180bcb1610
geanemr <geane.ramos@contavoltz.com>
2023-12-04 16:05:20 -0300

    chore: displaying tasks

--- src/Components/Column/Column.jsx
+++ src/Components/Column/Column.jsx
@@ -7,8 +7,9 @@ const Column = ({state}) => {
   const tasks = useStore(store=>store.tasks.filter(task=>task.state === state))
   return (
     <div className='column'>
-      <div>{state}</div>
-      <Task title="Todo"/>
+      <p>{state}</p>
+      {tasks.map((task)=><Task title={task.title} key={task.title}/>)}
+      
     </div>
   )
 }
