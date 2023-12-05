c263253f56766d6e09ba1a6f04f43cf251f4f71c
geanemr <geane.ramos@contavoltz.com>
2023-12-05 14:53:42 -0300

    chore: deleting tasks

--- store.js
+++ store.js
@@ -2,8 +2,14 @@ import { create } from "zustand";
 
 const store = (set) => ({
   tasks: [{ title: "TestTask", state: "PLANNED" }],
+
   addTask: (title, state) =>
     set((store) => ({ tasks: [...store.tasks, { title, state }] })), //adding a new element
+
+  deleteTask: (title) =>
+    set((store) => ({
+      tasks: store.tasks.filter((task) => task.title !== title),
+    })),
 });
 
 export const useStore = create(store);
